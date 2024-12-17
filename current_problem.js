2501708e26d32478af8899ee35ff7d9726633726
Joseph <joseph.fellow@yourfellowdesigns.com>
2024-12-17 07:59:13 -0500

    Update through completed 22

---
+++ current_problem.js
@@ -0,0 +1,20 @@
+/**
+ * Non-abundant Sums
+ * Perfect number = sum of its proper divisors is equal to the number itself.
+ * 
+ * deficient number = sum of its proper divisors is less than the number itself.
+ * abundant number = sum of its proper divisors is greater than the number itself.
+ * 
+ * 
+ * lowest limit of the sum of two abundant numbers is 24. because 12 is smallest.
+ * 
+ * upper limit is 28123. because 28123 is the largest number that can't be written as the sum of two abundant numbers. For some reason.
+ * 
+ * Find the sum of all the positive integers which cannot be written as the sum of two abundant numbers.
+ */
+
+// Find all the abundant numbers from 12 to 28123.
+
+function isAbundant(num) {
+  
+}
